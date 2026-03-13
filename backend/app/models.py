ff1e591a5812a9bb39f21aeac7a8314b11189a2f
Muhammad Baqir <mbaqir856@gmail.com>
2026-03-13 06:43:44 -0700

    Define Guardian model class and create relationship with student, many->many

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,8 +1,9 @@
 import uuid
+from datetime import date
 from enum import Enum as PyEnum
 from typing import Optional
 
-from sqlalchemy import String, Enum as SQLEnum, UUID, ForeignKey
+from sqlalchemy import Column, String, Enum as SQLEnum, UUID, ForeignKey, Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .database import Base
 
@@ -10,17 +11,6 @@ class UserRole(str, PyEnum):
     ADMIN = "admin"
     PROVIDER = "provider"
     EMPLOYEE = "employee"
-
-## User
-# email
-# password
-# pin
-# facility name
-# facility address
-# provider id
-# First name
-# Last name
-# Role
 
 class User(Base):
     __tablename__ = "users"
@@ -44,23 +34,18 @@ class User(Base):
 
     students: Mapped[list["Student"]] = relationship(back_populates="provider")
 
-# Child:
-# First Name
-# Last Name
-# picture
-# Nickname (optional)
-# Child ID (optional)
-# Gender
-# Date of Birth
-# Medical Note
-# Parents
-# Attendance
+guardian_student_association = Table(
+    "guardian_student_association",
+    Base.metadata,
+    Column("student_id", ForeignKey("students.id"), primary_key=True),
+    Column("guardian_id", ForeignKey("guardians.id"), primary_key=True),
+)
 
 class Student(Base):
     __tablename__ = "students"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    public_id: Mapped[uuid.UUID] = mapped_column(UUID (as_uuid=True), default=uuid.uuid4, unique=True, index=True)
+    public_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, index=True)
 
     # foreign key to link to user/provider
     provider_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True)
@@ -70,18 +55,36 @@ class Student(Base):
     nickname: Mapped[Optional[str]] = mapped_column(String(100))
     profile_picture: Mapped[Optional[str]] = mapped_column(String(255))
 
+    date_of_birth: Mapped[date] = mapped_column()
+    gender: Mapped[str] = mapped_column(String(20))
+
     medical_note: Mapped[Optional[str]] = mapped_column(String(350))
     state_id: Mapped[Optional[str]] = mapped_column(String(50))
 
     # Provider Relationshp=ip
     provider: Mapped["User"] = relationship(back_populates="students")
+    # parents relationship
+    guardians: Mapped[list["Guardian"]] = relationship(
+        secondary=guardian_student_association, 
+        back_populates="students"
+    )
 
+class Guardian(Base):
+    __tablename__ = "guardians"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    public_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, index=True)
 
+    first_name: Mapped[str] = mapped_column(String(100))
+    last_name: Mapped[str] = mapped_column(String(100))
 
-# Full Name (First Name, Last Name)
-# Picture (Allows upload, defaults if not provided)
-# Date of Birth (Format: 05/12/2023)
-# Nickname/Preferred Name (Optional)
-# State/Facility ID (Optional for legal/subsidy tracking)
-# Gender (Selection: male, female, n/a)
-# Allergy/Medical Note (Free-text field)
+    phone_number: Mapped[str] = mapped_column(String(20), index=True)
+    email: Mapped[Optional[str]] = mapped_column(String(255), unique=True, index=True)
+    hashed_pin: Mapped[str] = mapped_column(String(255))
+
+    residential_address: Mapped[Optional[str]] = mapped_column(String(255))
+
+    students: Mapped[list["Student"]] = relationship(
+        secondary=guardian_student_association, 
+        back_populates="guardians"
+    )
